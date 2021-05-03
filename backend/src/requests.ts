ca12c793016b55324b098f36d65c58c284e49875
timtoronto634 <tim225x@gmail.com>
2021-05-03 17:45:09 +0900

    [fix] import errors

--- backend/src/requests.ts
+++ backend/src/requests.ts
@@ -1,22 +1,23 @@
-import { vision } from '@google-cloud/vision';
+import vision from "@google-cloud/vision";
 // send image to Vision API
 
-export const requestGoogleVisionAPI = (image: string): object =>  {
+export const requestGoogleVisionAPI = async (
+  image: string
+): Promise<object> => {
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
 
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+  // const fileName = 'Local image file, e.g. /path/to/image.png';
+  const fileName = image;
 
-    /**
-     * TODO(developer): Uncomment the following line before running the sample.
-     */
-    // const fileName = 'Local image file, e.g. /path/to/image.png';
-    const fileName = image;
+  // Performs text detection on the local file
+  const [result] = await client.textDetection(fileName);
+  const detections = result.textAnnotations;
+  console.log("Text:");
+  detections.forEach((text) => console.log(text));
 
-    // Performs text detection on the local file
-    const [result] = await client.textDetection(fileName);
-    const detections = result.textAnnotations;
-    console.log('Text:');
-    detections.forEach(text => console.log(text));
-
-    return {};
-}
+  return {};
+};
