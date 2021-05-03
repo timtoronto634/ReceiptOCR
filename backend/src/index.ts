b2170427bd31258b81fc4834fabcf231731a6ef0
timtoronto634 <tim225x@gmail.com>
2021-05-03 18:12:41 +0900

    [add] visionAPI result

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import { handleHttpRequest } from "./ocr";
-import * as fs from "fs";
+import fs from "fs";
 
 const main = () => {
-  const convertedImageFolderPath = "../../images/converteds/";
+  //   const convertedImageFolderPath = "../../images/converteds/";
   const sampleRequest = {
     data: {
       name: "mybasket_black_bkgr",
@@ -11,13 +11,14 @@ const main = () => {
   };
   const requestData = sampleRequest;
   const imageName = requestData.data.name;
-  const imageCvtName = imageName + "_cvt";
-  const imageCvtPath = convertedImageFolderPath + imageCvtName;
-  if (!fs.existsSync(imageCvtPath)) {
-    // save base64 image
-    fs.writeFileSync(imageCvtPath, requestData.data.image);
-  }
-  const result = handleHttpRequest(imageCvtPath);
+  //   const imageCvtName = imageName + "_cvt";
+  //   const imageCvtPath = convertedImageFolderPath + imageCvtName;
+  //   if (!fs.existsSync(imageCvtPath)) {
+  //     // save base64 image
+  //     fs.writeFileSync(imageCvtPath, requestData.data.image);
+  //   }
+  const originalImageFolderPath = "../images/originals/";
+  const result = handleHttpRequest(originalImageFolderPath + imageName);
   console.log("result ======", result);
   return;
 };
--- backend/src/ocr.ts
+++ backend/src/ocr.ts
@@ -3,18 +3,20 @@ import { requestGoogleVisionAPI } from "./requests";
 
 // do ocr
 
-export const triggerOcr = (base64ImagePath: string): object => {
-  const imageBase64 = fs.readFileSync(base64ImagePath, "utf8");
-
+export const triggerOcr = (imagePath: string): object => {
   // get image from imagePath
-  const response: object = requestGoogleVisionAPI(imageBase64);
+  //   const response: object = requestGoogleVisionAPI(imagePath);
+  // mock response
+  const response: object = JSON.parse(
+    fs.readFileSync("result_saved.json", "utf8")
+  );
   console.log(response);
   const result = response;
   return result;
 };
 
-export const handleHttpRequest = (base64ImagePath: string) => {
-  triggerOcr(base64ImagePath);
+export const handleHttpRequest = (imagePath: string) => {
+  triggerOcr(imagePath);
 };
 
 // write here if you have your own OCR engine
