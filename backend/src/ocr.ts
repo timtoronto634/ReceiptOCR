6dc03005a24719c3311aef08a72cb8f70d31ba76
timtoronto634 <34266036+timtoronto634@users.noreply.github.com>
2021-05-04 10:30:04 +0900

    開発環境の整備: npm packageのインストール、auto linting (#11)
    * added node-version for nodenv.
    initialized npm
    
    * [add] gitignore
    
    * [add] linting

--- backend/src/ocr.ts
+++ backend/src/ocr.ts
@@ -1,23 +1,20 @@
-import fs from 'fs';
-import { requestGoogleVisionAPI } from './requests';
+import * as fs from "fs";
+import { requestGoogleVisionAPI } from "./requests";
 
 // do ocr
 
-export const triggerOcr = (base64ImagePath: string): object => {   
-    const imageBase64 = fs.readFileSync(base64ImagePath, 'utf8');
+export const triggerOcr = (base64ImagePath: string): object => {
+  const imageBase64 = fs.readFileSync(base64ImagePath, "utf8");
 
-    // get image from imagePath
-    const response: object = requestGoogleVisionAPI(imageBase64);
-    console.log(response);
-    const result = response;
-    return result;
-}
+  // get image from imagePath
+  const response: object = requestGoogleVisionAPI(imageBase64);
+  console.log(response);
+  const result = response;
+  return result;
+};
 
 export const handleHttpRequest = (base64ImagePath: string) => {
-    triggerOcr(base64ImagePath)   
-}
-
-
-
+  triggerOcr(base64ImagePath);
+};
 
 // write here if you have your own OCR engine
